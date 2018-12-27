ccb8707d1900e165f47ef5425f199a526baa866c
Rocío Daniela Fernandez Canales <rofernandez@belatrix.com>
2018-12-27 11:37:13 -0500

    First Commit with PageObject Pattern

---
+++ Training QA Automation/Training QA Automation/Framework/Actions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_QA_Automation.Framework
+{
+    class Actions
+    {
+        public void ClickOn()
+        {
+
+        }
+
+        public void Type()
+        {
+
+        }
+    }
+}
