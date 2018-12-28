e649e37fc423dfdefc3251ef27edcd126ab3bb2e
Rocío Daniela Fernández Canales <rociofc0312@gmail.com>
2018-12-28 14:40:35 -0500

    SauceLabs Configuration (#3)
    * SauceLabs Configuration
    
    * SauceLabs Configuration v2

--- Training QA Automation/Training QA Automation/Tests/RegistrationTest.cs
+++ Training QA Automation/Training QA Automation/Tests/RegistrationTest.cs
@@ -1,31 +1,51 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Training_QA_Automation.Framework.PageObjects;
 
 namespace Training_QA_Automation.Framework.Tests
 {
     [TestClass]
-    class RegistrationTest
+    public class RegistrationTest
     {
+        private IWebDriver driver;
+        private RegistrationPage registrationPage;
+        public TestContext TestContext { get; set; }
+
+
         [TestInitialize]
         public void SetUp()
         {
+            var sauceUserName = Environment.GetEnvironmentVariable("SAUCE_USERNAME", EnvironmentVariableTarget.User);
+            var sauceAccessKey = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY", EnvironmentVariableTarget.User);
+            ChromeOptions options = new ChromeOptions();
+            options.AddAdditionalCapability(CapabilityType.Version, "latest", true);
+            options.AddAdditionalCapability(CapabilityType.Platform, "Windows 10", true);
+            options.AddAdditionalCapability("username", sauceUserName, true);
+            options.AddAdditionalCapability("accessKey", sauceAccessKey, true);
+            options.AddAdditionalCapability("name", TestContext.TestName, true);
 
+            this.driver = new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), options.ToCapabilities(), TimeSpan.FromSeconds(600));
+            this.registrationPage = new RegistrationPage(driver);
         }
 
         [TestCleanup]
         public void CleanUp()
         {
-
+            var passed = TestContext.CurrentTestOutcome == UnitTestOutcome.Passed;
+            ((IJavaScriptExecutor)driver).ExecuteScript("sauce:job-result=" + (passed ? "passed" : "failed"));
+            driver?.Quit();
         }
 
         [TestMethod]
         public void RegistrationMethod()
         {
-
+            registrationPage.GotToPage();
+            registrationPage.ClickSignIn();
+            registrationPage.ClickCreateAccount();
+            Assert.AreEqual("Next", driver.FindElement(By.ClassName("btn-primary")).Text);
         }
     }
 }
